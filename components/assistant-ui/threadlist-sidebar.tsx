3db05106761eea3e64b9ea5b16cf40c1f445603b
Amaan-pathan <pathan.amaan@adypu.edu.in>
2025-11-14 21:11:03 +0530

    github link added

--- components/assistant-ui/threadlist-sidebar.tsx
+++ components/assistant-ui/threadlist-sidebar.tsx
@@ -24,7 +24,7 @@ export function ThreadListSidebar({
             <SidebarMenuItem>
               <SidebarMenuButton size="lg" asChild>
                 <Link
-                  href="https://assistant-ui.com"
+                  href="https://github.com/Amaan-pathan/Chatting"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -51,7 +51,7 @@ export function ThreadListSidebar({
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link
-                href="https://github.com/Amaan-pathan/Intellix_ai"
+                href="https://github.com/Amaan-pathan/Chatting"
                 target="_blank"
               >
                 <div className="aui-sidebar-footer-icon-wrapper flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
